11f5bf5c286f39aad418c677c42d0236798777b7
Sazib-Banik <sazib.banik.1@gmail.com>
2023-01-08 21:50:52 +0600

    email js add

--- js/main.js
+++ js/main.js
@@ -24,3 +24,46 @@ $('.play').on('click',function(){
 $('.stop').on('click',function(){
     owl.trigger('stop.owl.autoplay')
 })
+
+// form section
+const submit_btn = document.getElementById("submit_massage");
+submit_btn.addEventListener("click", function(){
+    const getfNameValue = document.getElementById("fName").value;
+    const getlNameValue = document.getElementById("lName").value;
+    const getEmailValue = document.getElementById("email").value;
+    const getPhoneValue = document.getElementById("phone").value;
+    const getMessageValue = document.getElementById("message").value;
+
+    // const fullName = getfNameValue + " " + getlNameValue;
+    // console.log(fullName);
+    
+
+    if(getfNameValue && getlNameValue && getEmailValue && getPhoneValue && getMessageValue){
+        const fullName = getfNameValue + " " + getlNameValue;
+        var templateParams = {
+          fullName: fullName,
+          getEmailValue: getEmailValue,
+          getPhoneValue: getPhoneValue,
+          getMessageValue: getMessageValue,
+        };
+        //console.log(fullName, getEmailValue, getPhoneValue, getMessageValue);
+        emailjs.send(
+          "service_portfolio",
+          "template_cjrhqw3",
+          templateParams,
+          "xWge5c0RcxQsvGhf4"
+        );
+    }
+})
+
+
+
+
+
+
+
+
+
+
+
+
